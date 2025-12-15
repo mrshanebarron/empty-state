f99693a4c1694aa7fcf30adaff4ae0bd6365f109
Shane Barron <shane@Mac.hsd1.fl.comcast.net>
2025-12-15 18:58:11 -0500

    Fix PHP syntax - use PascalCase for all class names

--- src/EmptyStateServiceProvider.php
+++ src/EmptyStateServiceProvider.php
@@ -3,14 +3,36 @@
 namespace MrShaneBarron\EmptyState;
 
 use Illuminate\Support\ServiceProvider;
+use MrShaneBarron\EmptyState\Livewire\EmptyState;
+use MrShaneBarron\EmptyState\View\Components\EmptyState as BladeEmptyState;
 
 class EmptyStateServiceProvider extends ServiceProvider
 {
+    public function register(): void
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../config/sb-empty-state.php', 'sb-empty-state');
+    }
+
     public function boot(): void
     {
-        if (class_exists(\Livewire\Livewire::class)) {
-            \Livewire\Livewire::component('sb-empty-state', Livewire\EmptyState::class);
-        }
         $this->loadViewsFrom(__DIR__ . '/../resources/views', 'sb-empty-state');
+
+        if (class_exists(\Livewire\Livewire::class)) {
+            \Livewire\Livewire::component('sb-empty-state', EmptyState::class);
+        }
+
+        $this->loadViewComponentsAs('ld', [
+            BladeEmptyState::class,
+        ]);
+
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../config/sb-empty-state.php' => config_path('sb-empty-state.php'),
+            ], 'sb-empty-state-config');
+
+            $this->publishes([
+                __DIR__ . '/../resources/views' => resource_path('views/vendor/sb-empty-state'),
+            ], 'sb-empty-state-views');
+        }
     }
 }
